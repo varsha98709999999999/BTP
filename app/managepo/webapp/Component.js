c803b601a7afba7e6613d06852442e37c1a89f3f
Varsha Takale <varshatakale0712@gmail.com>
2025-07-08 14:48:55 +0000

    build

--- app/managepo/webapp/Component.js
+++ app/managepo/webapp/Component.js
@@ -3,6 +3,7 @@ sap.ui.define(
     function (Component) {
         "use strict";
 
+        
         return Component.extend("bosch.po.managepo.Component", {
             metadata: {
                 manifest: "json"
